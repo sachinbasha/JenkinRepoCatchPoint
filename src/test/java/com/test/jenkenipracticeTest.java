2556dc1174d3fe46c73a675f9f9f363119d0e6d7
user <user@DESKTOP-EULJV3S>
2024-01-25 15:25:36 +0530

    a

---
+++ src/test/java/com/test/jenkenipracticeTest.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class jenkenipracticeTest {
+
+	@Test
+	public void aTest() {
+		Reporter.log("ok iam present",true);
+	}
+}
