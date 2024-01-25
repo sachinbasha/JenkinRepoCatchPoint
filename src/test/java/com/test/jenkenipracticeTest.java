afc0382f9c1dde9bcaf4647c0d546645b15c14ef
user <user@DESKTOP-EULJV3S>
2024-01-25 16:23:35 +0530

    c

--- src/test/java/com/test/jenkenipracticeTest.java
+++ src/test/java/com/test/jenkenipracticeTest.java
@@ -9,4 +9,19 @@ public class jenkenipracticeTest {
 	public void aTest() {
 		Reporter.log("ok iam present",true);
 	}
+	
+	@Test(groups="smoke")
+	public void aTest1() {
+		Reporter.log("ok iam smoke",true);
+	}
+	
+	@Test(groups="smoke")
+	public void aTest2() {
+		Reporter.log("ok iam smoke 2",true);
+	}
+	@Test(groups="regression")
+	public void aTest3() {
+		Reporter.log("ok iam regression 2",true);
+	}
+	
 }
