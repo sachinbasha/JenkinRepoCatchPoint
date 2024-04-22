e7cdc6d8ec382247ed2d101abcc9a12b74cfffa3
sachinbasha <uchesachin96@gmail.com>
2024-04-22 17:39:56 +0530

    njj

--- src/test/java/jenkins12/JenkinPracticeTest.java
+++ src/test/java/jenkins12/JenkinPracticeTest.java
@@ -5,14 +5,20 @@ import org.testng.annotations.Test;
 public class JenkinPracticeTest {
 
 	
-	@Test
+	@Test(groups = "smoke")
 	public void sachinTest()
 	{
-		System.out.println("sachinZenkinTest");
+		System.out.println("sachinZenkinTestsmoke1");
 	}
 	@Test
 	public void sachinTest1()
 	{
 		System.out.println("sachinZenkinTest");
+	}
+	
+	@Test(groups = "smoke")
+	public void sachinTest2()
+	{
+		System.out.println("sachinZenkinTestsmoke2");
 	}
 }
