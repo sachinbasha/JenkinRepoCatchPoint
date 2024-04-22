137211a3a5251b07adfed3ff74233f00358cc890
sachinbasha <uchesachin96@gmail.com>
2024-04-22 17:13:16 +0530

    hi jenkin i came

---
+++ src/test/java/jenkins/JenkinPracticeTest.java
@@ -0,0 +1,13 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+
+public class JenkinPracticeTest {
+
+	
+	@Test
+	public void sachinTest()
+	{
+		System.out.println("sachinZenkinTest");
+	}
+}
